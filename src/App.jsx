81a3fc00dfb186d02b0fd1ed7021290d97ed21bf
wook2964 <tiavns8554@gamil.com>
2026-02-09 11:46:46 +0900

    useEffect를 사용한 생명주기 관리 구현 (마운트, 특정 값 업데이트, 언마운트)

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function Counter() {
   // 1. 컴포넌트가 최초 렌더링 되는 경우에 실행되는 useEffect 함수를 구현하세요. (마운트)
   useEffect(() => {
     console.log("컴포넌트가 마운트됨!");
-  });
+  }, []);
 
   // 2. 컴포넌트가 리렌더링 되는 경우에 실행되는 useEffect 함수를 구현하세요. (업데이트)
   useEffect(() => {
@@ -33,11 +33,14 @@ function Counter() {
   // 3. counter2 값이 변경되는 경우에 실행되는 useEffect 함수를 구현하세요. (특정 값 업데이트)
   useEffect(() => {
     console.log("counter2 값이 변경됨");
-  });
+  }, [counter2]);
 
   // 4. 클린업 함수를 작성하세요. (언마운트)
-  useEffect(() => {});
-
+  useEffect(() => {
+    return () => {
+      console.log("컴포넌트가 언마운트됨!");
+    };
+  }, []);
   return (
     <section>
       <div>
